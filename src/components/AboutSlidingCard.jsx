2175f103158e8bb4b3d62b5144488e58a7dc9c1f
joseph <langj6757@gmail.com>
2024-05-20 22:16:38 -0700

    Change animation in about sliding card

--- src/components/AboutSlidingCard.jsx
+++ src/components/AboutSlidingCard.jsx
@@ -18,8 +18,10 @@ const AboutSlidingCard = ({ handleClose, isVisible }) => {
   return (
     <div
       className={`${
-        isVisible ? "translate-x-0" : "translate-x-[-200vw]"
-      }  max-w-[500px] fixed transition-all duration-700 h-full overflow-y-auto  bg-white p-5 flex flex-col gap-5 text-black z-50 ease-in-out`}
+        isVisible
+          ? "translate-x-0 max-w-[500px] fixed transition-all duration-500 h-full overflow-y-auto  bg-white p-5 flex flex-col gap-5 text-black z-50"
+          : "translate-x-[-200vw] max-w-[500px] fixed transition-all duration-500 h-full overflow-y-auto  bg-white p-5 flex flex-col gap-5 text-black z-50"
+      }  `}
     >
       <div className="flex justify-between items-center">
         <Logo color="black" />
